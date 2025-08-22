bee49f2717f506014fc472d51d1d062c2af97b34
recaiguel <133678256+recaiguel@users.noreply.github.com>
2025-08-22 23:51:51 +0200

    added HTTPS require via helmet

--- myApp.js
+++ myApp.js
@@ -53,6 +53,8 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
