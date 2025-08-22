13d9cc639006726941fb8899fa263ff07f23b70b
recaiguel <133678256+recaiguel@users.noreply.github.com>
2025-08-23 00:19:47 +0200

    added helmet.noCache middleware

--- myApp.js
+++ myApp.js
@@ -56,6 +56,8 @@ app.use(helmet.ieNoOpen());
 const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+
 const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
